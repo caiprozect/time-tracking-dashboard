67a2961bbc1003ef90eac5463413fbf05c496456
Caiprozect <caiprozect@gmail.com>
2022-01-29 16:54:01 +0900

    Style font-family

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Dashboard from '../components/Dashboard'
 
 export default function Home() {
   return (
-    <div className="min-w-screen min-h-screen bg-[#0f1424] py-20">
+    <div className="min-w-screen min-h-screen bg-[#0f1424] py-20 font-Rubik">
       <Head>
         <title>Time Tracking Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Rubik: ['Rubik', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
